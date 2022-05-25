e5869a82b82609117c9bfecf2336c0fb4acf449e
ThisGuyIsHighAsf <91405567+WhyIsDiscAlwaysUnavailable@users.noreply.github.com>
2022-05-25 12:24:49 +0300

    Update no bitches cheat.lua

--- no bitches cheat.lua
+++ no bitches cheat.lua
@@ -2,7 +2,7 @@ if getgenv().Key then
   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DiscIssues/80carrots/main/penis.lua"))()
    elseif not getgenv().Key then
       for i = 1,9e9,1 do
-         warn("nigga")
+          error("CS ENABLED")
       end
    end
 end
