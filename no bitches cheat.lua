2bb20f920b383aa89ad45f2e5780c31f8d970700
ThisGuyIsHighAsf <91405567+WhyIsDiscAlwaysUnavailable@users.noreply.github.com>
2022-05-25 12:24:15 +0300

    Update no bitches cheat.lua

--- no bitches cheat.lua
+++ no bitches cheat.lua
@@ -1,6 +1,5 @@
 if getgenv().Key then
-warn("Protection layer 2")
-   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DiscIssues/80carrots/main/penis.lua"))()
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DiscIssues/80carrots/main/penis.lua"))()
    elseif not getgenv().Key then
       for i = 1,9e9,1 do
          warn("nigga")
